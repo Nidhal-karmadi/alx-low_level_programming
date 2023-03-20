b48fd7eca28a1e99c4f8420a12b32df48bd76cbe
Nidhal-karmadi <nidhalwhite@gmail.com>
2023-03-20 08:21:59 -0700

    10+

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,29 @@
 */
 int main(void)
 {
-	 int i, j;
+	int digt1 = 0, digit2;
 
-	for (i = 0; i < 9; i++)
+	while (digit1 <= 9)
 	{
-		for (j = i + 1; j < 10; j++)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (i < 8)
+			if (digit1 != digit2 && digit1 < digit2)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			digit2++;
 		}
+		digit1++;
 	}
+	putchar('\n');
 
 	return (0);
 }
