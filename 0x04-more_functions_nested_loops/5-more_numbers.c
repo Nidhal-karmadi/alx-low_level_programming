b07e9d60cd9a41b0a6066f09168a1a60ecf4f657
Nidhal-karmadi <nidhalwhite@gmail.com>
2023-03-25 09:01:40 -0700

    5-

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, ch;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 14; j++)
+		for (ch = 0; ch < 15; ch++)
 		{
-		if (j >= 10)
-		_putchar('1');
-	_putchar((j % 10) + '0');
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
-}
+
 
